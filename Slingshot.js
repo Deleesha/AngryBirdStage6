92bc2b1d22a144a2bab7de4847d7435186e60100
Deleesha <jaindeleesha@gmail.com>
2020-10-12 15:55:55 +0530

    vanishing pigs

--- Slingshot.js
+++ Slingshot.js
@@ -18,6 +18,10 @@ class SlingShot{
         this.sling.bodyA = null;
     }
 
+    attach(mybody){
+        this.sling.bodyA = mybody;
+    }
+
     display(){
         image(this.sling1,200,20);
         image(this.sling2,170,20);
